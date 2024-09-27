690214faed3132464c5cba9aa6ede0820302d584
Bhavana-Mallineni <mallinenibhavana123@gmail.com>
2024-09-27 16:54:15 +0530

    Adding routes to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <News />
+        <Router> 
+          <NavBar />
+        
+          <Routes>
+              <Route path='/' element={<News key="general" pageSize={5} country={'us'} category={'general'} />} />
+              <Route exact path='/business' element={<News keu="business" pageSize={5} country={'us'} category={'business'} />} />
+              <Route path='/entertainment' element={<News key="entertainment" pageSize={5} country={'us'} category={'entertainment'} />} />
+              <Route path='/general' element={<News key="general" pageSize={5} country={'us'} category={'general'} />} />
+              <Route path='/health' element={<News key="health" pageSize={5} country={'us'} category={'health'}/> } />
+              <Route path='/science' element={<News key="science" pageSize={5} country={'us'} category={'science'} />} />
+              <Route path='/sports' element={<News key="sports" pageSize={5} country={'us'} category={'sports'} />} />
+              <Route path='/technology' element={<News key="technology" pageSize={5} country={'us'} category={'technology'} />} /> 
+          </Routes>
+        </Router>
+        
       </div>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
 
+    static defaultProps = {
+        country: 'in',
+        pageSize: 8,
+        category: 'general',
+    }
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
     articles = []
 
     constructor(){
@@ -17,28 +30,33 @@ export class News extends Component {
 
     async componentDidMount() {
         console.log("cdm");
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=e5844dd20c394df6966e7ed01d84929c&page=1&pageSize=20"
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e5844dd20c394df6966e7ed01d84929c&page=1&pageSize=${this.props.pageSize}`
+        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({articles: parsedData.articles, totalArticles: parsedData.totalResults});
+        this.setState({articles: parsedData.articles, totalArticles: parsedData.totalResults,
+            loading: false
+        });
         console.log(parsedData.totalResults);
     }
 
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=e5844dd20c394df6966e7ed01d84929c&page=${this.state.page - 1}&pageSize=20`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e5844dd20c394df6966e7ed01d84929c&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
+        this.setState({loading: false});
         this.setState({page: this.state.page - 1,
             articles: parsedData.articles})        
     }
 
     handleNextClick = async () => {
-        if(this.state.page  > Math.ceil(this.state.totalArticles/20)){
-
-        }else {
-            let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=e5844dd20c394df6966e7ed01d84929c&page=${this.state.page + 1}&pageSize=20`
+        if(!(this.state.page  > Math.ceil(this.state.totalArticles/20))){
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=e5844dd20c394df6966e7ed01d84929c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
+        this.setState({loading: false});
         this.setState({articles: parsedData.articles, 
             page: this.state.page + 1
         })
@@ -49,12 +67,12 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-4'>
-
-       <h2>The Chronicles - Top Headlines</h2>
+        <h1 className='text-center' style={{margin: '30px 0px'}}>The Chronicles - Top Headlines</h1>
+        { this.state.loading && < Spinner />} 
        
        <div className="row">
-        { this.state.articles.map((ele)=> {
-           
+        { !this.state.loading && this.state.articles.map((ele)=> {
+                if(ele.title !== '[Removed]' && ele.description !== '[Removed]' )
                  return <div className='col-md-4' key={ele.url}>
                 <NewsItem  title={ele.title? ele.title : " "}  description={ele.description ? ele.description : " "} imageUrl = {ele.urlToImage ? ele.urlToImage : "https://img.freepik.com/premium-photo/news-lettering-blue-background_391052-5080.jpg" } newsUrl={ele.url} />
             </div>
@@ -64,7 +82,7 @@ export class News extends Component {
 
             <div className="container d-flex justify-content-between">
             <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-            <button type="button" disabled={this.state.page +1 > Math.ceil(this.state.totalArticles/20)}className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+            <button type="button" disabled={this.state.page +1 > Math.ceil(this.state.totalArticles/this.props.pageSize)}className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
             </div>
             
         </div>
